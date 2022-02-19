3a55640dee65f5113759d5106379cdc1d9da6659
xing_cx <caoxing9@gmail.com>
2022-02-19 15:00:00 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kc-mc-package
 管理部署工具打包webpack插件
+# 使用
+```
+    // 安装
+    npm install kc-mc-package -d -s
+    // vue.config.js
+    const kcp = require('kc-mc-package')
+    ...
+    plugins: [
+        new kcp({
+            projectName: 'dist'
+        })
+    ]
+```
---
+++ src/index.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const path = require('path')
+const yazl = require('yazl')
+const crypto = require('crypto')
+class KcMcPackage {
+    constructor(opt) {
+        this.opt = opt
+    }
+    apply(compiler) {
+        const { projectName } = this.opt
+        compiler.plugin('afterEmit', async (comilation, cb) => {
+            console.log(
+                '\n============================压缩文件=========================='
+            )
+            const zipFile = new yazl.ZipFile()
+            const zipFile2 = new yazl.ZipFile()
+
+            for (const name in comilation.assets) {
+                let source = comilation.assets[name].source()
+                zipFile.addBuffer(Buffer.from(source), name)
+            }
+            zipFile.end()
+            await new Promise((resolve) => {
+                zipFile.outputStream.pipe(
+                    fs.createWriteStream(`dist.zip`).on('close', () => {
+                        const buffer = fs.readFileSync(
+                            path.join(__dirname, `dist.zip`)
+                        )
+                        const hash = crypto.createHash('md5')
+                        hash.update(buffer, 'utf8')
+                        const md5 = hash.digest('hex')
+                        const str = `${md5} ${projectName}.zip`
+                        fs.writeFile(
+                            `./${projectName}.txt`,
+                            str,
+                            function (err) {
+                                if (err) {
+                                    // 写入文件失败
+                                } else {
+                                    // 写入文件成功
+                                    resolve('结束')
+                                }
+                            }
+                        )
+                    })
+                )
+            })
+            await new Promise((resolve) => {
+                zipFile2.addFile(`dist.zip`, `${projectName}.zip`)
+                zipFile2.addFile(`${projectName}.txt`, `${projectName}.txt`)
+                zipFile2.end()
+                zipFile2.outputStream.pipe(
+                    fs
+                        .createWriteStream(`${projectName}.zip`)
+                        .on('close', () => {
+                            console.log('打包完成')
+                            resolve()
+                        })
+                )
+            })
+
+            fs.unlink(`./${projectName}.txt`, () => {})
+            fs.unlink(`./dist.zip`, () => {})
+        })
+    }
+}
+
+module.exports = KcMcPackage;
